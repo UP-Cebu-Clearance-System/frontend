e9e36794b3ed671433cba79893aef040022fa3da
Justin Po <justin.andre.po@gmail.com>
2020-07-08 00:14:32 +0800

    added initial login screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React, { lazy } from 'react'
 
 // Components
 import { Suspense } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import { ThemeProvider } from 'theme-ui'
 import { Provider as StoreProvider } from 'react-redux'
 
 import clearance from 'styles/clearance'
 import store from 'redux/store'
 
+const Login = lazy(() => import('./routes/Login'))
 const Home = lazy(() => import('./routes/Home'))
 const Flows = lazy(() => import('./routes/Flows'))
 const Pending = lazy(() => import('./routes/Pending'))
@@ -20,9 +21,11 @@ function App() {
         <Router>
           <Suspense fallback={<div>Loading...</div>}>
             <Switch>
-              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/home" component={Home} />
               <Route exact path="/admin" component={Flows} />
               <Route exact path="/office" component={Pending} />
+              <Redirect to="/login" component={Login} />
             </Switch>
           </Suspense>
         </Router>
--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,28 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 // Components
-import { Container, Box } from 'theme-ui'
-import { FlowCard, AddFlowCard, ModifyFlowCard } from 'components/Cards'
+import { Container, Box, Flex, Button, Text } from 'theme-ui'
 import Navigation from 'components/Navigation'
+import { Input } from 'components/Forms'
 
 function Flows() {
-  const gridStyle = {
-    display: 'grid',
-    gridGap: 4,
-    gridTemplateColumns: ['1fr', '1fr 1fr', '1fr 1fr 1fr'],
-    mt: 4,
+  const flexStyle = {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 'calc(100vh - 58px)',
+  }
+
+  const boxStyle = {
+    border: '1px solid #DEDEDE',
+    borderRadius: '4px',
   }
 
   return (
     <>
       <Navigation />
       <Container>
-        <Box sx={gridStyle}>
-          <FlowCard />
-          <FlowCard />
-          <AddFlowCard />
-        </Box>
-        <ModifyFlowCard />
+        <Flex sx={flexStyle}>
+          <Box sx={boxStyle}>
+            <Box>Login</Box>
+            <Flex>
+              <Button>Student</Button>
+              <Button>Approver</Button>
+            </Flex>
+            <Box>
+              <Input placeholder="ID" />
+              <Input placeholder="Password" type="password" />
+            </Box>
+            <Button>Login</Button>
+            <Flex>
+              <Text>New Account?</Text>
+              <Link>Sign up Here</Link>
+            </Flex>
+          </Box>
+        </Flex>
       </Container>
     </>
   )
